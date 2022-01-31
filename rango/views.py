6ce106f7fe4a2a42091d0e8e985d31d157d62360
Lucas Clement <2745633c@student.gla.ac.uk>
2022-01-31 22:49:46 +0000

    Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
-    # Not the key boldmessage matches to {{ boldmessage }} in the template!
+    # Now the key boldmessage matches to {{ boldmessage }} in the template!
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
     # Return a rendered repsponse to send to the client
@@ -12,5 +12,4 @@ def index(request):
     return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href = " + \
-                        "'/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
